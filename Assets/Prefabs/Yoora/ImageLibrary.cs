d8f737cdd4764bf7349b18588363ee946a468b7c
yooksj13 <yooksj13@ewhain.net>
2023-06-10 20:18:19 +0900

    중간 스테이지들
    앞선 stage 끝나고 stage 변수 바뀌면 다음 스테이지 정해진 후 UI까지

--- Assets/Prefabs/Yoora/ImageLibrary.cs
+++ Assets/Prefabs/Yoora/ImageLibrary.cs
@@ -12,47 +12,32 @@ public class ImageLibrary : MonoBehaviour
     [SerializeField]
     private GameObject[] uiObjects; // 각 이미지에 대응하는 비활성화된 UI 게임 오브젝트들을 저장할 배열
     private Dictionary<string, GameObject> spawnedUis = new Dictionary<string, GameObject>(); // 추적하고 있는 이미지와 연결된 게임 오브젝트들을 담는 딕셔너리
+    private HashSet<string> detectedImages = new HashSet<string>(); // 이미지를 인식한 이미지의 이름을 저장하는 집합
 
     //fadein,out 관련
     //fade 시간 inspector에서 설정 가능
     public float fadeTime = 1.5f;
     private float accumTime = 0f;
 
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnEnable()
     {
-        trackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+        trackedImageManager.trackedImagesChanged += OnTrackedImagesChanged; // 이미지 추적 상태 변화 이벤트에 대한 핸들러 등록
     }
 
     private void OnDisable()
     {
-        trackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+        trackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged; // 이미지 추적 상태 변화 이벤트 핸들러 제거
     }
 
     private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
         foreach (var trackedImage in eventArgs.added)
         {
-
-
-
-            UpdateImage(trackedImage);
-
-
-
+            if (!detectedImages.Contains(trackedImage.referenceImage.name))
+            {
+                detectedImages.Add(trackedImage.referenceImage.name); // 이미지 이름을 detectedImages 집합에 추가
+                UpdateImage(trackedImage); // 이미지 업데이트 함수 호출
+            }
         }
     }
 
@@ -62,13 +47,8 @@ public class ImageLibrary : MonoBehaviour
         GameObject trackedUi = spawnedUis[name]; // 추적 중인 이미지와 연결된 UI를 가져옴
         CanvasGroup canvasGroup = trackedUi.GetComponent<CanvasGroup>();
 
-        
-            StartCoroutine(FadeIn(canvasGroup, 5f));
-        
-        
+        StartCoroutine(FadeIn(canvasGroup, 5f)); // 페이드 인 코루틴 실행
     }
-
-
 
     private IEnumerator FadeIn(CanvasGroup canvasGroup, float waitTime)
     {
@@ -82,7 +62,7 @@ public class ImageLibrary : MonoBehaviour
         // Gradually fade in
         while (accumTime < fadeTime)
         {
-            canvasGroup.alpha = Mathf.Lerp(originalAlpha, targetAlpha, accumTime / fadeTime);
+            canvasGroup.alpha = Mathf.Lerp(originalAlpha, targetAlpha, accumTime / fadeTime); // 페이드 인 애니메이션
             yield return null;
             accumTime += Time.deltaTime;
         }
@@ -92,7 +72,7 @@ public class ImageLibrary : MonoBehaviour
         yield return new WaitForSeconds(waitTime);
 
         // Call the FadeOut coroutine
-        StartCoroutine(FadeOut(canvasGroup));
+        StartCoroutine(FadeOut(canvasGroup)); // 페이드 아웃 코루틴 실행
     }
 
     private IEnumerator FadeOut(CanvasGroup canvasGroup)
@@ -105,11 +85,10 @@ public class ImageLibrary : MonoBehaviour
         // Gradually fade out
         while (accumTime < fadeTime)
         {
-            canvasGroup.alpha = Mathf.Lerp(originalAlpha, targetAlpha, accumTime / fadeTime);
+            canvasGroup.alpha = Mathf.Lerp(originalAlpha, targetAlpha, accumTime / fadeTime); // 페이드 아웃 애니메이션
             yield return null;
             accumTime += Time.deltaTime;
         }
         canvasGroup.alpha = targetAlpha;
     }
-
 }
---
+++ Assets/Prefabs/Yoora/MiddleStage.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiddleStage : MonoBehaviour
+{
+    public CanvasGroup[] panels; // UI 패널들을 담을 배열
+
+    private int stage = 0; // 현재 스테이지 변수
+    private bool isStage2Activated = false; // stage 2에서 활성화된 패널 여부 확인을 위한 변수
+
+    private float fadeTime = 1f; // 페이드 시간 설정
+    private float accumTime; // 경과 시간
+
+    private void Update()
+    {
+        if (stage == 2 && !isStage2Activated)
+        {
+            ActivateRandomPanel(panels.Length);
+            isStage2Activated = true;
+        }
+        else if (stage == 3 && isStage2Activated)
+        {
+            ActivateRandomPanel(panels.Length - 1, stage);
+            isStage2Activated = false;
+        }
+    }
+
+    private void ActivateRandomPanel(int panelCount, int excludedPanelIndex = -1)
+    {
+        // 모든 패널 비활성화
+        foreach (CanvasGroup panel in panels)
+        {
+            panel.alpha = 0f;
+        }
+
+        // 활성화할 패널 인덱스
+        int panelIndex;
+
+        if (excludedPanelIndex != -1)
+        {
+            // 제외할 패널 인덱스를 제외하고 랜덤한 패널 활성화
+            panelIndex = Random.Range(0, excludedPanelIndex);
+            if (panelIndex >= excludedPanelIndex)
+            {
+                panelIndex += 1;
+            }
+        }
+        else
+        {
+            // 모든 패널 중 랜덤한 패널 활성화
+            panelIndex = Random.Range(0, panelCount);
+        }
+
+        // 선택한 패널에 FadeIn 애니메이션 실행
+        CanvasGroup selectedPanel = panels[panelIndex];
+        StartCoroutine(FadeIn(selectedPanel, 0f));
+    }
+
+    private IEnumerator FadeIn(CanvasGroup canvasGroup, float waitTime)
+    {
+        yield return new WaitForSeconds(0.2f);
+        accumTime = 0f;
+
+        float originalAlpha = canvasGroup.alpha;
+        float targetAlpha = 1f;
+
+        // Gradually fade in
+        while (accumTime < fadeTime)
+        {
+            canvasGroup.alpha = Mathf.Lerp(originalAlpha, targetAlpha, accumTime / fadeTime); // 페이드 인 애니메이션
+            yield return null;
+            accumTime += Time.deltaTime;
+        }
+        canvasGroup.alpha = targetAlpha;
+
+        // Keep it visible for the specified wait time
+        yield return new WaitForSeconds(waitTime);
+
+        // Call the FadeOut coroutine
+        StartCoroutine(FadeOut(canvasGroup)); // 페이드 아웃 코루틴 실행
+    }
+
+    private IEnumerator FadeOut(CanvasGroup canvasGroup)
+    {
+        accumTime = 0f;
+
+        float originalAlpha = canvasGroup.alpha;
+        float targetAlpha = 0f;
+
+        // Gradually fade out
+        while (accumTime < fadeTime)
+        {
+            canvasGroup.alpha = Mathf.Lerp(originalAlpha, targetAlpha, accumTime / fadeTime); // 페이드 아웃 애니메이션
+            yield return null;
+            accumTime += Time.deltaTime;
+        }
+        canvasGroup.alpha = targetAlpha;
+    }
+}
